a2bdb9688c32c8702db1cb67d2de128e1e9796bc
Ty N <1196438+ty-n-42@users.noreply.github.com>
2023-04-25 17:16:32 +1000

    swapped from apply impulse once, at ready(); to apply force every physics simulation cycle.

--- levels/player.gd
+++ levels/player.gd
@@ -3,14 +3,18 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var direction: Vector2 = Vector2.RIGHT;
-	var magnitude: int = 25;
-	
-	apply_impulse(direction * magnitude);
+	pass;
 
 
 #
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-#
+	print(Engine.get_frames_per_second());
+
+
+# Called every physics simulation cycle, 'delta' is the elapsed time since the previous cycle.
+func _physics_process(delta):
+	var direction: Vector2 = Vector2.RIGHT;
+	var magnitude: int = 25;
+	
+	apply_force(direction * magnitude);
