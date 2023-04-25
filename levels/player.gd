e3f5737e41b52322edc4d710d32f00fb41b385fd
Ty N <1196438+ty-n-42@users.noreply.github.com>
2023-04-25 16:46:24 +1000

    created function parameters

--- levels/player.gd
+++ levels/player.gd
@@ -3,21 +3,13 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	test();
+	print_doubled(10);
 
 
-func test():
-	print("testing a new function");
-	print(10);
-	print(3.142);
-	print(10 / 3);
-	print(10 / 3.0);
-	print(10.0 / 3);
-	print(10.0 / 3.0);
-	print("ten " + str(10) + ", five and a half " + str(5.5));
-	var value: int = 2 + 3 + 5;
-	print(value);
-
+func print_doubled(number: int) -> void:
+	print(number * 2);
+	
+	
 #
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
