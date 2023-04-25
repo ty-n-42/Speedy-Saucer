4966ff02dc69e03969f5437f7c1be91e40c2ab1b
Ty N <1196438+ty-n-42@users.noreply.github.com>
2023-04-25 17:04:59 +1000

    applied an instantaneous impulse to the player

--- levels/player.gd
+++ levels/player.gd
@@ -3,15 +3,14 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print_doubled(10);
+	var direction: Vector2 = Vector2.RIGHT;
+	var magnitude: int = 25;
+	
+	apply_impulse(direction * magnitude);
 
 
-func print_doubled(number: int) -> void:
-	print(number * 2);
-	
-	
 #
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
 #
